4f886ca6537a10dbca00b99eef8ccd73c1e0249b
Arvir-S <s31396@caseygrammar.vic.edu.au>
2026-03-26 01:02:53 +0000

    Completed Task2

--- Task2.py
+++ Task2.py
@@ -1,10 +1,11 @@
 # Task 2: James Bond
 def main():
     #Write your code for input here
-    
+    firstname = input('What is your first name? ')
+    lastname = input('What is your last name? ')
+    print(f'My name is {lastname}, {firstname} {lastname}')
 
 
     # End of your code for input here
-
- if __name__ == '__main__':
+if __name__ == '__main__':
     main()
